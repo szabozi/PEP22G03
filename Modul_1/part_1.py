e34741720ce9dc2039d164605a14d3f40351dea6
szabozi <szabozi@yahoo.com>
2022-05-31 21:25:04 +0300

    part1

--- Modul_1/part_1.py
+++ Modul_1/part_1.py
@@ -1,1 +1,29 @@
-print('Hello World!')
+# print('Hello World!', 'My Object', end='\n', sep=' - abcd - ')
+#
+# print(print)
+#
+# print('###', '# #', '###', sep='\n')
+#
+# print(type('Hello World'))
+#
+# print('3')
+# print(3)
+#
+# print(type(3))
+
+print(3 + 3) # - * / +
+print('3' + '3') # only + for str
+print('3' * 5) # 33333
+
+print('#'*12)
+print('#',' '*8,'#')
+print('#'*12)
+
+my_first_value = 12
+my_second_value = 'this is a string'
+my_third_value = 13
+
+print(my_first_value)
+print(my_second_value)
+print(my_first_value + my_third_value)
+
