ff16732d5f7f8e6645f21a721bde981f6ab7f82a
szabozi <szabozi@yahoo.com>
2022-06-02 21:41:47 +0300

    day2

---
+++ Modul_2/day_2.py
@@ -0,0 +1,301 @@
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+
+
+
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+
+
+
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+
+
+
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+
+
+
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+
+
+
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+
+
+
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+
+
+
+# INTEGER
+
+number = 3
+print(number)
+print(type(number))
+
+# FLOAT
+
+number = 3.1
+print(number)
+print(type(number))
+
+# CASTING
+
+number = int(3.1)
+print(number)
+print(type(number))
+
+# BOOLEAN: true / false
+
+a = 4
+b = 5
+print(a > b)  # false
+print(a < b)  # true
+bool_object_1 = False
+bool_object_2 = True
+print(type(bool_object_1))
+
+print('Convert to bool', bool(1))
+print('Convert to bool', int(True))
+print('Convert to bool', bool(0))
+print('Convert to bool', int(False))
+
+
+# locatie de memorie
+
+print('Memory address is:', id(bool_object_1))
+print('Memory address is:', id(number))
+
+# operatii aritmetice
+# +  -  * -> suma, scadere, inmultire
+# a ** b -> a la puterea b
+# a / b -> returneaza float
+# a // b -> returneaa int
+# a % b -> returneaza restul impartirii
+
+#operatii relationali cu tipuri de date numerice
+# is - verifica identitatea obiectului
+# is not - verifica identitatea negata a obiectului
+# <
+# <=
+# >
+# >=
+#
+
+#print('Prob.1')
+#b=int(input('Enter value for b: '))
+#h=int(input('Enter value for h:'))
+#a=1/2*b*h
+#print('Rezultat: ',a)
+
+#print('Prob.2')
+#passw = 7710
+#passinput = int(input('enter password: '))
+#print(passw == passinput)
+
+#print('prob.3')
+#a = int(input('Enter value 1: '))
+#b = int(input('Enter Value 2: '))
+#media = (a + b) / 2
+#impartire = a // b
+#putere = a ** b
+#print('Media numerelor este: ', media)
+#print('Impartirea numerelor este: ', impartire)
+#print('a la puterea b este: ', putere)
+
+#print('prob.4')
+#venit = int(input('Venit:'))
+#print('50% = ',(50/100)*venit)
+#print('30% = ',(30/100)*venit)
+#print('20% = ',(20/100)*venit)
+
+# complex numbers
+
+myNumber = 1 + 4j
+print(type(myNumber))
+print(myNumber)
+
+#radical
+myNumber = (-4)**(1/2)
+print(myNumber)
